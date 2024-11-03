5df0612a675093a1760fbe17232cc0f6567da14d
TheSilverbirdSage <138786450+TheSilverbirdSage@users.noreply.github.com>
2024-11-03 15:35:03 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div>
       <p>admin dash</p>
+             <Link href="/admin">
+        <button className="p-2 bg-blue-500 text-white rounded mt-4">
+          Go to Admin Page
+        </button>
+      </Link>
+
     </div>
   );
 }
